a1032fc46eea68f196502dc418dcff9d2ef99468
Arthur <arthur.romanchev@yandex.ru>
2020-02-20 23:46:59 +0300

    end

--- main/models.py
+++ main/models.py
@@ -4,9 +4,8 @@ class Artist(models.Model):
 	title = models.CharField(max_length=200)
 	title_image = models.ImageField(upload_to='media/title_image', null=True)
 	text = models.TextField()
-	background_image = models.ImageField(upload_to='media/background-image', null=True)
+	background_image = models.ImageField(upload_to='media/background-image',null=True)
 	link = models.URLField(null=True, blank=True)
-	list_back = models.ImageField(upload_to='media/background-image', null=True)
 
 	def publish(self):
 		self.save()
